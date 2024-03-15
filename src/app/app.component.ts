488cd76f6b5523f51845e093f7a6bc53b61f4d51
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2024-03-15 11:44:31 +0530

    reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular16-playground';
+
+  userForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      firstName: [null, [Validators.required, Validators.minLength(4)]],
+      lastName: [null, [Validators.required, Validators.maxLength(8)]],
+    });
+  }
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submitHandler() {
+    console.log('loginForm', this.loginForm);
+
+    this.loginForm.reset();
+  }
+
+  userFormHandler() {
+    console.log('userform, status', this.userForm?.status);
+    console.log('userform, value', this.userForm?.value);
+  }
 }
